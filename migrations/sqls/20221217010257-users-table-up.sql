4e263bea5c73f6ad95029938c64b3e57a40b89cb
AbdulazizAssainar <abdlaziz.a.abdullah@gmail.com>
2022-12-18 16:26:27 +0200

    a

--- migrations/sqls/20221217010257-users-table-up.sql
+++ migrations/sqls/20221217010257-users-table-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     firstName VARCHAR(50),
     lastName VARCHAR(50),
-    password VARCHAR(MAX)
+    password VARCHAR(500)
     );
--- migrations/sqls/20221217023747-order-products-table-down.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE order_products (
-    id SERIAL PRIMARY KEY,
-    product REFERENCES products(id),
-    quantity integer,
-    order_id REFERENCES orders(id),
-    );
